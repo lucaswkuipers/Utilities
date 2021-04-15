9152c2619c2622c118490f4158c1cab93256bd9c
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-04-15 17:27:31 -0300

    :sparkles: Added example of coin converting

--- ExampleUtilities/ExampleUtilities/ViewController.swift
+++ ExampleUtilities/ExampleUtilities/ViewController.swift
@@ -9,19 +9,33 @@ import UIKit
 import Utilities
 
 class ViewController: UIViewController {
+	
+	@IBOutlet weak var doubleToConvertTextField: UITextField!
+	@IBOutlet weak var moneyConvertedLabel: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		setupUI()
+
+
 		let idList = ["123", "321"]
-		
 		let encodedIDList = Utilities.encode(idList: idList)
 		let decodedIDList = Utilities.decode(idListString: encodedIDList)
 		let formatted = Utilities.formatCoin(coinAmount: 123456789.123456789)
 		
 		// Do any additional setup after loading the view.
 	}
-
+	
+	func setupUI() {
+		doubleToConvertTextField.delegate = self
+		doubleToConvertTextField.addTarget(self, action: #selector(ViewController.textFieldDidChange(_:)), for: .editingChanged)
+	}
 
 }
 
+extension ViewController: UITextFieldDelegate {
+	@objc func textFieldDidChange(_ textField: UITextField) {
+		moneyConvertedLabel.text = Utilities.formatCoin(coinAmount: Double(textField.text!) ?? 0.00)
+	}
+}
